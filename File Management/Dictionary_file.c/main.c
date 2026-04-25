651537f5fc39d94f29998c4870c6cf189ded91c5
ripon <ripon@cse.nits.ac.in>
2026-04-25 21:12:10 +0530

    Updating File Management programs

--- File Management/Dictionary_file.c/main.c
+++ File Management/Dictionary_file.c/main.c
@@ -1,3 +1,7 @@
+/*****************************************************
+Dictionary program: Dictionary is loaded from file.
+******************************************************/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
